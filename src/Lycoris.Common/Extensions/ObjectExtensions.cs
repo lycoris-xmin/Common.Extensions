81efdc223d70a25fa6f9a872a13bae7c1305b617
Lycoris <zzyo.yj@outlook.com>
2024-09-05 16:21:22 +0800

    1

--- src/Lycoris.Common/Extensions/ObjectExtensions.cs
+++ src/Lycoris.Common/Extensions/ObjectExtensions.cs
@@ -10,6 +10,8 @@ using Lycoris.Common.Extensions.Extensions;
 using Lycoris.Common.Extensions.Extensions.Models;
 */
 using Lycoris.Common.Extensions.Models;
+using Newtonsoft.Json;
+using System.Collections;
 using System.Reflection;
 
 namespace Lycoris.Common.Extensions
@@ -273,5 +275,190 @@ namespace Lycoris.Common.Extensions
             if (dic.ContainsKey(key))
                 dic.Remove(key);
         }
+
+        //public static T ToObject<T>(IDictionary dict) where T : class, new()
+        //{
+        //    T obj = new T();
+        //    if (dict == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    return ToObject(dict.Keys.Cast<string>().ToDictionary((string key) => key, (string key) => dict[key]), obj);
+        //}
+
+        //public static T ToObject<T>(Dictionary<string, object> dict, T obj) where T : class
+        //{
+        //    if (dict == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    PropertyInfo[] properties = obj.GetType().GetProperties();
+        //    foreach (PropertyInfo propertyInfo in properties)
+        //    {
+        //        Type propertyType = propertyInfo.PropertyType;
+        //        string key = ((!(propertyInfo.GetCustomAttribute(typeof(NameInMapAttribute)) is NameInMapAttribute nameInMapAttribute)) ? propertyInfo.Name : nameInMapAttribute.Name);
+        //        if (dict.ContainsKey(key))
+        //        {
+        //            object obj2 = dict[key];
+        //            if (obj2 == null)
+        //            {
+        //                propertyInfo.SetValue(obj, obj2);
+        //            }
+        //            else
+        //            {
+        //                propertyInfo.SetValue(obj, MapObj(propertyType, obj2));
+        //            }
+        //        }
+        //    }
+
+        //    return obj;
+        //}
+
+        //private static object MapObj(Type propertyType, object value)
+        //{
+        //    if (value == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    if (propertyType.Equals(typeof(string)))
+        //    {
+        //        if (typeof(IList).IsAssignableFrom(value.GetType()) || typeof(IDictionary).IsAssignableFrom(value.GetType()))
+        //        {
+        //            return JsonConvert.SerializeObject(value);
+        //        }
+
+        //        return Convert.ToString(value);
+        //    }
+
+        //    if (typeof(IList).IsAssignableFrom(value.GetType()) && !typeof(Array).IsAssignableFrom(value.GetType()))
+        //    {
+        //        object obj = Activator.CreateInstance(propertyType);
+        //        Type[] genericArguments = propertyType.GetGenericArguments();
+        //        if (genericArguments.Length == 0 || genericArguments == null)
+        //        {
+        //            return value;
+        //        }
+
+        //        Type type = genericArguments[0];
+        //        {
+        //            foreach (object item in (IList)value)
+        //            {
+        //                MethodInfo method = propertyType.GetMethod("Add", new Type[1] { type });
+        //                if (method != null)
+        //                {
+        //                    if (item == null)
+        //                    {
+        //                        method.Invoke(obj, new object[1]);
+        //                        continue;
+        //                    }
+
+        //                    object obj2 = MapObj(type, item);
+        //                    method.Invoke(obj, new object[1] { obj2 });
+        //                }
+        //            }
+
+        //            return obj;
+        //        }
+        //    }
+
+        //    if (typeof(TeaModel).IsAssignableFrom(propertyType))
+        //    {
+        //        object obj3 = Activator.CreateInstance(propertyType);
+        //        return ToObject(((IDictionary)value).Keys.Cast<string>().ToDictionary((string key) => key, (string key) => ((IDictionary)value)[key]), obj3);
+        //    }
+
+        //    if (typeof(IDictionary).IsAssignableFrom(propertyType))
+        //    {
+        //        IDictionary dictionary = (IDictionary)value;
+        //        if (dictionary.Count == 0)
+        //        {
+        //            return dictionary;
+        //        }
+
+        //        IDictionary dictionary2;
+        //        if (propertyType.Equals(typeof(IDictionary)))
+        //        {
+        //            dictionary2 = dictionary;
+        //        }
+        //        else
+        //        {
+        //            dictionary2 = (IDictionary)Activator.CreateInstance(propertyType);
+        //            Type propertyType2 = propertyType.GetGenericArguments()[1];
+        //            foreach (DictionaryEntry item2 in dictionary)
+        //            {
+        //                if (item2.Value == null)
+        //                {
+        //                    dictionary2.Add(item2.Key, null);
+        //                    continue;
+        //                }
+
+        //                item2.Value.GetType();
+        //                dictionary2.Add(item2.Key, MapObj(propertyType2, item2.Value));
+        //            }
+        //        }
+
+        //        return dictionary2;
+        //    }
+
+        //    if (propertyType.Equals(typeof(object)))
+        //    {
+        //        return value;
+        //    }
+
+        //    if (propertyType.Equals(typeof(int)) && value is long)
+        //    {
+        //        return Convert.ToInt32((long)value);
+        //    }
+
+        //    if (propertyType == typeof(int?))
+        //    {
+        //        return Convert.ToInt32(value);
+        //    }
+
+        //    if (propertyType == typeof(long?))
+        //    {
+        //        return Convert.ToInt64(value);
+        //    }
+
+        //    if (propertyType == typeof(float?))
+        //    {
+        //        return Convert.ToSingle(value);
+        //    }
+
+        //    if (propertyType == typeof(double?))
+        //    {
+        //        return Convert.ToDouble(value);
+        //    }
+
+        //    if (propertyType == typeof(bool?))
+        //    {
+        //        return Convert.ToBoolean(value);
+        //    }
+
+        //    if (propertyType == typeof(short?))
+        //    {
+        //        return Convert.ToInt16(value);
+        //    }
+
+        //    if (propertyType == typeof(ushort?))
+        //    {
+        //        return Convert.ToUInt16(value);
+        //    }
+
+        //    if (propertyType == typeof(uint?))
+        //    {
+        //        return Convert.ToUInt32(value);
+        //    }
+
+        //    if (propertyType == typeof(ulong?))
+        //    {
+        //        return Convert.ToUInt64(value);
+        //    }
+
+        //    return Convert.ChangeType(value, propertyType);
+        //}
     }
 }
